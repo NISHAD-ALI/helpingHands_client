3b33cba50759f285870e854c4da56465b459930e
nishad <nishadalichenadan@gmail.com>
2024-07-13 15:49:13 +0530

    final commit 36

--- src/Pages/Volunteer/HomePage.tsx
+++ src/Pages/Volunteer/HomePage.tsx
@@ -103,7 +103,7 @@ const HomePage:React.FC = () => {
     <div className='bg-gradient-to-br from-teal-50 to-green-200 font-inter'>
       <NavBar />
       <HeroSection
-        imageSrc="/public/joel-muniz-A4Ax1ApccfA-unsplash.jpg"
+        imageSrc="../joel-muniz-A4Ax1ApccfA-unsplash.jpg"
         title="You make the world a"
         title1="better place."
         description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries, but also the leap into electronic It has survived not only five centuries, but also the leap into electronic Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic"
