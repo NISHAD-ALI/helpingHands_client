d2b29f36ee0fbbd203a063d65c131bd22a949b81
nishad <nishadalichenadan@gmail.com>
2024-07-13 10:17:14 +0530

    final commit 34

--- src/Components/UserComponents/FounderMessage.tsx
+++ src/Components/UserComponents/FounderMessage.tsx
@@ -8,7 +8,7 @@ const FounderMessage: React.FC = () => {
             </div>
             <div className="mb-4">
                 <img
-                    src="/assets/nishad1.jpg"
+                    src="./nishad1.jpg"
                     alt="Founder"
                     className="w-80 h-80 rounded-full object-cover"
                 />
