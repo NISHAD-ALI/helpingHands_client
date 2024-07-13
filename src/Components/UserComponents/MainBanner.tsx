6043e9f4c168ace347cdf473fd6b72eb31460a1b
nishad <nishadalichenadan@gmail.com>
2024-07-13 09:54:47 +0530

    final commit22

--- src/Components/UserComponents/MainBanner.tsx
+++ src/Components/UserComponents/MainBanner.tsx
@@ -8,7 +8,7 @@ const MainBanner:React.FC = () => {
           Help a hand,<br/>
           Make a change
         </h1>
-        <img src="/public/Vector 32.png" alt="Vector 32" width={400}/>
+        <img src="../Vector 32.png" alt="Vector 32" width={400}/>
         <p className='text-sm font-bold text-center'>
         "Join our community-driven platform where compassion meets<br/> action.Together, we're a force for good, bringing hope, support,<br/> and empowerment to those in need. Join us and make an impact <br/>today."
         </p>
