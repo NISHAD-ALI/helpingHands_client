44d3d801e580f83d829a7f07369a4602f10ae47e
nishad <nishadalichenadan@gmail.com>
2024-09-09 23:08:10 +0530

    after editing ui in fundraiser

--- src/Config/AxiosInstance.ts
+++ src/Config/AxiosInstance.ts
@@ -1,17 +1,17 @@
-// import axios, { AxiosInstance } from "axios";
-
-// const axiosInstance : AxiosInstance = axios.create({
-//     baseURL : "https://helpinghands.hisandhersfashion.shop",
-//     withCredentials : true
-// })
-
-// export default axiosInstance
-
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance : AxiosInstance = axios.create({
-    baseURL : "http://localhost:3000",
+    baseURL : "https://helpinghands.hisandhersfashion.shop",
     withCredentials : true
 })
 
 export default axiosInstance
+
+// import axios, { AxiosInstance } from "axios";
+
+// const axiosInstance : AxiosInstance = axios.create({
+//     baseURL : "http://localhost:3000",
+//     withCredentials : true
+// })
+
+// export default axiosInstance
